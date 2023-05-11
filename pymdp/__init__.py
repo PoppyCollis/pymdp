380e120805c18e01f7fbad8f8341ab3e37fc511a
Poppy Collis <44060045+PoppyCollis@users.noreply.github.com>
2023-05-11 14:20:39 +0100

    Update __init__.py
    Added the imports from Paul's __init__.py of custom 'Tool create' environments

--- pymdp/__init__.py
+++ pymdp/__init__.py
@@ -7,3 +7,13 @@ from . import inference
 from . import learning
 from . import algos
 from . import default_models
+
+# Imports for tool environment
+from .env import Env
+from .grid_worlds import GridWorldEnv, DGridWorldEnv
+from .visual_foraging import VisualForagingEnv, SceneConstruction, RandomDotMotion, initialize_scene_construction_GM, initialize_RDM_GM
+from .tmaze import TMazeEnv, TMazeEnvNullOutcome
+from .tool_create import Tool_create
+from .tool_create_single import Tool_create_single
+from .tool_create_single_cut import Tool_create_single_cut
+from .tool_create_old import Tool_create_old
